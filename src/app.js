905b9270ae48fa5a3074c7b46d31f1dbb3c9453e
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2021-03-10 11:33:13 -0700

    Avance

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 const menuIcon = document.querySelector('.navbar__menu-icon');
 menuIcon.addEventListener('click', () => {
 
+    if (menuIcon.classList.contains('fa-bars')) {
+        menuIcon.classList.remove('fa-bars');
+        menuIcon.classList.add('fa-times');
+    } else if (menuIcon.classList.contains('fa-times')) {
+        menuIcon.classList.remove('fa-times');
+        menuIcon.classList.add('fa-bars')
+    }
+
+    document.querySelector('.navbar__list').classList.toggle('active');
+
 });
